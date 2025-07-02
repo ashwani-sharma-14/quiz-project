997843198a25bc48140ce8d3c936373d95f1bd7e
Vansh Shrivastava <143945626+vansh3007@users.noreply.github.com>
2025-07-02 12:49:19 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import app from "./app";
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
