01325bea7f1f57078f1417124fd3fa143170a506
vansh3007 <vanshshrivastava30@gmail.com>
2025-07-02 12:25:44 +0530

    build changes

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://placement-quiz-practice-portal.netlify.app",
+    ],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import http from "http";
 import app from "./app";
 
+
 const server = http.createServer(app);
 
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
